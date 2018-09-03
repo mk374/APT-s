8654209b388eb6f992424dc7fb5576d574af611f
Min Soo Kim <mk374@duke.edu>
2018-09-03 01:39:15 -0400

    completed SandwichBar

--- SandwichBar.java
+++ SandwichBar.java
@@ -1,53 +1,46 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SandwichBar{
-      
-	
-	
-	
 	public int whichOrder(String[] available, String[] orders){
-		boolean hey = false;
-         for (int i=0; i< orders.length; i++) {
-        	 	
-        	 	
-        	 	while loop of available checking if ing is within availabe
-        	 		if even one is not, then it exists with the boolean becoming false
-        	 	
-        	 	if boolean of the previous while statement is true
-        	 		return i;
-        	 	
-        	 	String[] ing = orders[i].split(" ");
-        	 	boolean ingTruth = true;
-        	 	int ingC = 0;
-        	 	while(ingTruth) {
-        	 		for (int i = 0; i < available.length; i++) {
-        	 			for (int y =0; y< ing.length; y++) {
-        	 				if (available[i].equals(ing[y])) {
-        	 					
-        	 				}
-        	 			}
-        	 				
-        	 		}
-        	 		
-        	 		
-        	 		ingC++;
-        	 	}
-         }
-         int i = 0;
-         
-         
-         return -1;
-     }
-
-
-	public static void main(String[] args) {
-		String[] available = { "ham", "cheese", "mustard" };
-
-		String [] orders = { "ham", "cheese" };
 		
-		System.out.println(orders in available);
-
+		
+		for (int i= 0; i < orders.length; i++) {
+			//maximum number of true's an order can get
+			int maxScore = 0;
+			
+			//getting each individual order into a set with no duplicates
+			Set<String> or1 = new HashSet<String>();
+			//below line is splitting the respective order
+			String[] spl = orders[i].split(" ",0);
+			for (int k = 0; k < spl.length; k++) {
+				or1.add(spl[k]);
+			}
+			
+			//limit of the number of trues
+			int limit = or1.size();
+			// adding the number of trues
+			for(String s: or1) {
+				if(Arrays.asList(available).contains(s)) {
+					maxScore += 1;
+				}
+			}
+			if (maxScore == limit) {
+				return i;
+			}
+				
+		}
+		return -1;
+	
 	}
-
-
+	public static void main(String[] args) {
+		String[] available = { "foo", "bar", "baz", "gazonk", "quux", "bat", "xyzzy",
+				  "shme", "hukarz", "grault", "waldo", "bleh" };
+		String[] orders = { "kalatehas", "spam eggs", "needle haystack", "bleh blarg", "plugh", 
+		  "the best sandwich in the universe" };
+		SandwichBar x = new SandwichBar();
+		System.out.print(x.whichOrder(available, orders));
+	}
 }
